748b068c6742f523b3cb4c113e719c13b185f071
hanschrs <hans_christian@ymail.com>
2017-12-10 11:32:13 +0700

    ummm..

--- Assets/Script/MenuScript.cs
+++ Assets/Script/MenuScript.cs
@@ -15,8 +15,24 @@ public class MenuScript : MonoBehaviour {
 		
 	}
 
-	public void play() {
+	public void play1() {
 		SceneManager.LoadScene ("Play");	
+	}
+
+	public void play2() {
+		SceneManager.LoadScene ("Play2");	
+	}
+
+	public void play3() {
+		SceneManager.LoadScene ("Play3");	
+	}
+
+	public void play4() {
+		SceneManager.LoadScene ("Play4");	
+	}
+
+	public void play5() {
+		SceneManager.LoadScene ("Play5");	
 	}
 
 	public void exitGame(){
